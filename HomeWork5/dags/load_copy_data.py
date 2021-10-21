f4368a696df2b520f39879a14099535d7ca376ea
Viktor Andriichuk <vandriichuk@appen.com>
2021-10-21 18:43:58 +0300

    Fix some bugs

--- HomeWork5/dags/load_copy_data.py
+++ HomeWork5/dags/load_copy_data.py
@@ -1,12 +1,9 @@
-from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow import models
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-from utils.db import get_db_url
-import time
 from os import listdir
 from os.path import isfile, join
 
@@ -34,15 +31,13 @@ def load_from_db_and_save_to_file():
     for table_name in table_names_list:
 
         full_file_path = os.getcwd() + '/upload-data/' + table_name  + '.csv'
-        print(full_file_path)
-
-        sql = f"COPY {table_name} TO '{full_file_path}' DELIMITER ',' CSV HEADER;"
 
         pg_conn = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_conn()
         cursor = pg_conn.cursor()
-        cursor.execute(sql)
-        pg_conn.commit()
-        pg_conn.close()
+
+        with open(full_file_path, 'w') as f:
+            cursor.copy_to(f, table_name, sep=',')
+            pg_conn.commit()
 
 
 with DAG(dag_id=DAG_ID,
